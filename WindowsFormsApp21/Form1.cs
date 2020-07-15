3eaa0d26e76121881a2aebfd60aba5f246257e7d
Sl1ed <45339767+Sl1ed@users.noreply.github.com>
2020-07-15 10:16:19 +0400

    комментарии

--- WindowsFormsApp21/Form1.cs
+++ WindowsFormsApp21/Form1.cs
@@ -16,16 +16,19 @@ namespace WindowsFormsApp21
         public Form1()
         {
             InitializeComponent();
+            //создание объектов
             cirс = new circle[4];
             cirс[0] = new circle(new Point(0, 100), "Right", false, Brushes.Red);
             cirс[1] = new circle(new Point(1085, 200), "Left", true, Brushes.Blue);
             cirс[2] = new circle(new Point(0, 300), "Right", true, Brushes.Red);
             cirс[3] = new circle(new Point(1085, 400), "Left", true, Brushes.Blue);
+            //подписка на обработчик события
             cirс[0].stopm += cirс[1].StopMove;
             cirс[1].stopm += cirс[2].StopMove;
             cirс[2].stopm += cirс[3].StopMove;
             cirс[3].stopm += cirс[0].StopMove;
         }
+        //Рисование всех объектов
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             cirс[0].draw(e.Graphics);
@@ -33,25 +36,26 @@ namespace WindowsFormsApp21
             cirс[2].draw(e.Graphics);
             cirс[3].draw(e.Graphics);
         }
-
+        //Происходит по истечении заданного интервала таймера при условии, что таймер включен
         private void timer1_Tick(object sender, EventArgs e)
-        {
-            timer1.Enabled = true;
+        { 
             cirс[0].Move();
             cirс[1].Move();
             cirс[2].Move();
             cirс[3].Move();
             Invalidate();
         }
+        //запуск таймера
         private void стартToolStripMenuItem_Click(object sender, EventArgs timer1_Tick)
         {
             timer1.Start();
         }
-
+        //остановка таймера
         private void стопToolStripMenuItem_Click(object sender, EventArgs e)
         {
             timer1.Stop();
         }
+        //Класс кругов
         class circle
         {
             public Point position;
@@ -63,8 +67,8 @@ namespace WindowsFormsApp21
             string direction;
             public circle(Point beginposition, string Direction, bool Stop, Brush Color)
             {
-                this.color = Color;
-                this.size = new Size(100, 100);
+                color = Color;
+                size = new Size(100, 100);
                 position = beginposition;
                 direction = Direction;
                 stop = Stop;
@@ -106,7 +110,7 @@ namespace WindowsFormsApp21
                 stop = false;
             }
         }
-
+        //Информация о разработчике
         private void информацияОРазработчикеToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Выполнил: студент группы 4207 Антаев М.П.\nВариант: №3", "Информация о разработчике");
